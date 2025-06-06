e8f5e7b3149e6214ca42d30fefade939901b3f61
GiuseppeColucci95 <colucci539@gmail.com>
2025-06-06 11:14:05 +0200

    first commit

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,31 @@
+package org.lessons.java.shop;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Prodotto telefono = new Prodotto("iPhone", "Smartphone di alto livello", 1499.99f, 22);
+    Prodotto pc = new Prodotto("MacBook Pro", "laptop da utilizzare per lavoro", 2999.99f, 23);
+
+    System.out.println(telefono.codice);
+    System.out.println(telefono.nome);
+    System.out.println(telefono.descrizione);
+    System.out.println(telefono.prezzo);
+    System.out.println(telefono.iva);
+
+    System.out.println(telefono.getPrezzo());
+    System.out.println(telefono.getPrezzoWithIva());
+    System.out.println(telefono.getNomeEsteso());
+
+    System.out.println(pc.codice);
+    System.out.println(pc.nome);
+    System.out.println(pc.descrizione);
+    System.out.println(pc.prezzo);
+    System.out.println(pc.iva);
+
+    System.out.println(pc.getPrezzo());
+    System.out.printf("%.2f", pc.getPrezzoWithIva());
+    System.out.println("");
+    System.out.println(pc.getNomeEsteso());
+  }
+}
---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,38 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+
+  public int codice;
+  public String nome;
+  public String descrizione;
+  public float prezzo;
+  public int iva;
+
+  // constructor
+  public Prodotto(String nome, String descrizione, float prezzo, int iva) {
+
+    Random random = new Random();
+    this.codice = random.nextInt(1000);
+    this.nome = nome;
+    this.descrizione = descrizione;
+    this.prezzo = prezzo;
+    this.iva = iva;
+  }
+
+  public float getPrezzo() {
+    return this.prezzo;
+  }
+
+  public float getPrezzoWithIva() {
+    float getIva = (this.prezzo * this.iva) / 100;
+
+    return (this.prezzo + getIva);
+  }
+
+  public String getNomeEsteso() {
+
+    return codice + "-" + nome;
+  }
+}
