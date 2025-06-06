139b532e96e4670a8ab795fbb7dcfed35096ef37
GiuseppeColucci95 <colucci539@gmail.com>
2025-06-06 16:37:11 +0200

    refactor Prodotto class to use private fields and add getters; update Main class to reflect changes

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,23 +5,22 @@ public class Main {
   public static void main(String[] args) {
 
     Prodotto telefono = new Prodotto("iPhone", "Smartphone di alto livello", 1499.99f, 22);
-    Prodotto pc = new Prodotto("MacBook Pro", "laptop da utilizzare per lavoro", 2999.99f, 23);
+    Prodotto pc = new Prodotto("MacBook Pro", "Laptop da utilizzare per lavoro", 2999.99f, 23);
 
-    System.out.println(telefono.codice);
-    System.out.println(telefono.nome);
-    System.out.println(telefono.descrizione);
-    System.out.println(telefono.prezzo);
-    System.out.println(telefono.iva);
-
+    System.out.println(telefono.getCodice());
+    System.out.println(telefono.getNome());
+    System.out.println(telefono.getDescrizione());
     System.out.println(telefono.getPrezzo());
-    System.out.println(telefono.getPrezzoWithIva());
+    System.out.println(telefono.getIva());
+
+    System.out.printf("%.2f\n", telefono.getPrezzoWithIva());
     System.out.println(telefono.getNomeEsteso());
 
-    System.out.println(pc.codice);
-    System.out.println(pc.nome);
-    System.out.println(pc.descrizione);
-    System.out.println(pc.prezzo);
-    System.out.println(pc.iva);
+    System.out.println(pc.getCodice());
+    System.out.println(pc.getNome());
+    System.out.println(pc.getDescrizione());
+    System.out.println(pc.getPrezzo());
+    System.out.println(pc.getIva());
 
     System.out.println(pc.getPrezzo());
     System.out.printf("%.2f\n", pc.getPrezzoWithIva());
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Prodotto {
 
-  public int codice;
-  public String nome;
-  public String descrizione;
-  public float prezzo;
-  public int iva;
+  private int codice;
+  private String nome;
+  private String descrizione;
+  private float prezzo;
+  private int iva;
 
   // constructor
   public Prodotto(String nome, String descrizione, float prezzo, int iva) {
@@ -21,10 +21,55 @@ public class Prodotto {
     this.iva = iva;
   }
 
+  // second constructor
+  public Prodotto(String nome, String descrizione, float prezzo) {
+
+    Random random = new Random();
+    this.codice = random.nextInt(1000);
+    this.nome = nome;
+    this.descrizione = descrizione;
+    this.prezzo = prezzo;
+    this.iva = 22;
+  }
+
+  // getters and setters
+  public int getCodice() {
+    return this.codice;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescrizione() {
+    return this.descrizione;
+  }
+
+  public void setDescrizione(String descrizione) {
+    this.descrizione = descrizione;
+  }
+
   public float getPrezzo() {
     return this.prezzo;
   }
 
+  public void setPrezzo(float prezzo) {
+    this.prezzo = prezzo;
+  }
+
+  public int getIva() {
+    return this.iva;
+  }
+
+  public void setIva(int iva) {
+    this.iva = iva;
+  }
+
+  // other methods
   public float getPrezzoWithIva() {
     float getIva = (this.prezzo * this.iva) / 100;
 
